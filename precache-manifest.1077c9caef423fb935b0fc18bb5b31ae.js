9795b631af00fb340aa22d9952a8177749d4de7d
aquasar <arhoy@ualberta.ca>
2019-06-17 18:29:57 -0600

    Updates

---
+++ precache-manifest.1077c9caef423fb935b0fc18bb5b31ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe53ee74c9b565878bb00c10e2691b8",
+    "url": "/dev-connect-demo/index.html"
+  },
+  {
+    "revision": "798a3b502666be351e74",
+    "url": "/dev-connect-demo/static/css/main.fed7a7c6.chunk.css"
+  },
+  {
+    "revision": "36c68d140a8553eebb51",
+    "url": "/dev-connect-demo/static/js/2.f738b9b9.chunk.js"
+  },
+  {
+    "revision": "798a3b502666be351e74",
+    "url": "/dev-connect-demo/static/js/main.61ca6fb0.chunk.js"
+  },
+  {
+    "revision": "e8dc5564ae5d74674807",
+    "url": "/dev-connect-demo/static/js/runtime~main.f35f0f13.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/dev-connect-demo/static/media/showcase.4b31330b.jpg"
+  }
+]);
